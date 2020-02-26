8d0401c7961c8ddb97b9136d46c8f4ed54eced8b
eaprce <evan-price@uiowa.edu>
2020-02-26 11:07:46 -0600

    got timer working

--- test.py
+++ test.py
@@ -1,60 +1,45 @@
 import csv
 import random
-import threading
+import time
 #got scrabbles words from http://www.poslarchive.com/math/scrabble/lists/common-6.html
 
-def guess(guesses):
-    if guesses == 6:
-        print("\nO\n")
-    if guesses == 5:
-        print("\nO-\n")
-    if guesses == 4:
-        print("\nO-/\n")
-    if guesses == 3:
-        print("\nO->\n")
-    if guesses == 2:
-        print("\nO->-\n")
-    if guesses == 1:
-        print("\nO->-/\n")
-
 def scores(words, score, counter):
-    for i in range(0,len(words)):
-        print(words)
-        print(len(words))
-        helper0 =['e','a','i','o','n','r','t','l','s','u']
-        helper1 =['d','g']                
-        helper2 =['b','c','m','p']
-        helper3 =['f','h','v','w','y']
-        helper4 =['k']
-        helper5 =['j','x']                
-        helper7 =['q','z']
-        if words in helper0:
-            score = score + (1 * counter)
-            return score
-        if words in helper1:
-            score = score + 2 * counter
-            return score
-        if words in helper2:
-            score = score + 3 * counter
-            return score
-        if words in helper3:
-            score = score + 4 * counter
-            return score
-        if words in helper4:
-            score = score + 5 * counter
-            return score
-        if words in helper5:
-            score = score + 8 * counter
-            return score
-        if words in helper6:
-            score = score +10 * counter
-            return score
+    print(words)
+    print(len(words))
+    helper0 =['e','a','i','o','n','r','t','l','s','u']
+    helper1 =['d','g']                
+    helper2 =['b','c','m','p']
+    helper3 =['f','h','v','w','y']
+    helper4 =['k']
+    helper5 =['j','x']                
+    helper6 =['q','z']
+    if words in helper0:
+        score = score + (1 * counter)
+        return score
+    if words in helper1:
+        score = score + 2 * counter
+        return score
+    if words in helper2:
+        score = score + 3 * counter
+        return score
+    if words in helper3:
+        score = score + 4 * counter
+        return score
+    if words in helper4:
+        score = score + 5 * counter
+        return score
+    if words in helper5:
+        score = score + 8 * counter
+        return score
+    if words in helper6:
+        score = score +10 * counter
+        return score
 
-def scoretable(board, solution, blist, guesses):
-    #stopper = threading.Event()
-    scoreboard = 0
+def scoretable(board, solution, blist, scoreboard, timeout_start, len_soln):
     counter = 0
-    while guesses > 0:
+    x=0
+    while time.time() < timeout_start:
+        print(timeout_start + 1 - time.time())    
         if len(board) == 1:
             counter = 0
             if board in solution:
@@ -63,7 +48,6 @@ def scoretable(board, solution, blist, guesses):
                     if board == solspt[va]:
                         counter = counter + 1
                         blist[va] = board
-                guess(guesses)
                 for i in range(0, len(blist)):
                     print(blist[i], end = ' ')
                 scoreboard = scores(board, scoreboard, counter)
@@ -71,66 +55,62 @@ def scoretable(board, solution, blist, guesses):
                 counter = 0
                 if "_" not in blist:
                     print("success")
-                    return
-                board = str(input("\nGuess a letter:\nYou have " + str(guesses) + " incorrect guesses left\n"))
+                    timeout_start = timeout_start + len_soln
+                    return scoreboard
+                board = str(input("\nGuess a letter:\n"))
             else:
-                guesses -= 1
-                guess(guesses)
                 for i in range(0, len(blist)):
                     print(blist[i], end = ' ')
-                if guesses == 0:
-                    return
-                else:
-                    print("\nScore: " + str(scoreboard))
-                    board = str(input("\nIncorrect!\nYou have " + str(guesses) + " incorrect guesses left\n"))
         else:
-            guesses -= 1
             for i in range(0, len(blist)):
                 print(blist[i], end = ' ')
             print("\nScore: " + str(scoreboard))
-            board = str(input("\nIncorrect!\nYou have " + str(guesses) + " incorrect guesses left\n"))
-    if guesses == 0:
-        print("\nO->-< \nFAILURE, the word was " + solution)
-        print("\nScore: " + str(scoreboard))
+            board = str(input("\nIncorrect!\n"))
 
 wordamount = -1
+scoreboard = 0
+timeout_start = time.time() + 1
 while wordamount != 0:
     wordamount = random.randint(2,8)
-    if wordamount == 0:
-        break
-    guesses = 6
-    csv_reader = csv.reader(open('words.csv', mode='r'))
-    line_count = 0
-    colm_count = 0
-    va = 0
-    changer = []
-    solution = ""
-    #cnt = 0
-    for row in csv_reader:
-        line_count += 1
-        if len(list(row[1])) == wordamount:
-            changer.append(line_count)
+    len_soln=(5*wordamount)
+    while time.time() < timeout_start:# Petr Krampl  https://stackoverflow.com/questions/13293269/how-would-i-stop-a-while-loop-after-n-amount-of-time
+        timeout_start = timeout_start + len_soln
+        print(timeout_start + 1 - time.time())
+        if wordamount == 0:
+            break
+        csv_reader = csv.reader(open('words.csv', mode='r'))
+        line_count = 0
+        colm_count = 0
+        va = 0
+        changer = []
+        solution = ""
+        #cnt = 0
+        for row in csv_reader:
+            line_count += 1
+            if len(list(row[1])) == wordamount:
+                changer.append(line_count)
 
-    x = random.randint(min(changer), max(changer))
+        x = random.randint(min(changer), max(changer))
 
-    csv_reader = csv.reader(open('words.csv', mode='r'))
-    top = 0
-    i = 0
-    for col in csv_reader:
-        colm_count += 1
-        if colm_count == x:
-            if row[i] != " ":
-                top += 1
-            d = random.randint(0, (top-1))
-            solution = col[d]
-            print(col[d])
-
-    guess(guesses)
-    bord = ""
-    for i in range(0,wordamount):
-            bord += "_"
-            print("_ ", end = '')
-    blist = list(bord)
-    board = str(input("\nGuess a letter:\nYou have " + str(guesses) + " incorrect guesses left\n"))
-    scoretable(board, solution, blist, guesses)
+        csv_reader = csv.reader(open('words.csv', mode='r'))
+        top = 0
+        i = 0
+        for col in csv_reader:
+            colm_count += 1
+            if colm_count == x:
+                if row[i] != " ":
+                    top += 1
+                d = random.randint(0, (top-1))
+                solution = col[d]
+                print(col[d])
+        bord = ""
+        for i in range(0,wordamount):
+                bord += "_"
+                print("_ ", end = '')
+        blist = list(bord)
+        board = str(input("\nGuess a letter:\n"))
+        scoreboard = scoretable(board, solution, blist, scoreboard, timeout_start, len_soln)
+    if time.time() >= timeout_start + len_soln:
+        print("Game Over: \nFinal Score: " + str(scoreboard))
+    
 
