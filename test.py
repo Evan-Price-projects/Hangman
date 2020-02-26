3a4d7342ca710f5cc7ca22214a98f0c0c41451e8
eaprce <evan-price@uiowa.edu>
2020-02-26 10:10:56 -0600

    made it into functions

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import csv
 import random
+import threading
 #got scrabbles words from http://www.poslarchive.com/math/scrabble/lists/common-6.html
 
 def guess(guesses):
@@ -16,45 +17,83 @@ def guess(guesses):
     if guesses == 1:
         print("\nO->-/\n")
 
-def scores(words, score):
+def scores(words, score, counter):
     for i in range(0,len(words)):
         print(words)
         print(len(words))
-        helper0 =['h','u','n','a','i','l','o','t']
-        helper1 =['e','s']                
-        helper2 =['d','z']
-        helper3 =['k']
-        helper4 =['g','y']
-        helper5 =['w']                
-        helper6 =['b']
-        helper7 =['c','j','m']
+        helper0 =['e','a','i','o','n','r','t','l','s','u']
+        helper1 =['d','g']                
+        helper2 =['b','c','m','p']
+        helper3 =['f','h','v','w','y']
+        helper4 =['k']
+        helper5 =['j','x']                
+        helper7 =['q','z']
         if words in helper0:
-            score = score + 1
+            score = score + (1 * counter)
             return score
         if words in helper1:
-            score = score + 2
+            score = score + 2 * counter
             return score
         if words in helper2:
-            score = score + 3
+            score = score + 3 * counter
             return score
         if words in helper3:
-            score = score + 4
+            score = score + 4 * counter
             return score
         if words in helper4:
-            score = score + 5
+            score = score + 5 * counter
             return score
         if words in helper5:
-            score = score + 7
+            score = score + 8 * counter
             return score
         if words in helper6:
-            score = score + 8
-            return score
-        if words in helper7:
-            score = score +10
+            score = score +10 * counter
             return score
 
+def scoretable(board, solution, blist, guesses):
+    #stopper = threading.Event()
+    scoreboard = 0
+    counter = 0
+    while guesses > 0:
+        if len(board) == 1:
+            counter = 0
+            if board in solution:
+                solspt = list(solution)
+                for va in range(0, len(solspt)):
+                    if board == solspt[va]:
+                        counter = counter + 1
+                        blist[va] = board
+                guess(guesses)
+                for i in range(0, len(blist)):
+                    print(blist[i], end = ' ')
+                scoreboard = scores(board, scoreboard, counter)
+                print("\nScore: " + str(scoreboard))
+                counter = 0
+                if "_" not in blist:
+                    print("success")
+                    return
+                board = str(input("\nGuess a letter:\nYou have " + str(guesses) + " incorrect guesses left\n"))
+            else:
+                guesses -= 1
+                guess(guesses)
+                for i in range(0, len(blist)):
+                    print(blist[i], end = ' ')
+                if guesses == 0:
+                    return
+                else:
+                    print("\nScore: " + str(scoreboard))
+                    board = str(input("\nIncorrect!\nYou have " + str(guesses) + " incorrect guesses left\n"))
+        else:
+            guesses -= 1
+            for i in range(0, len(blist)):
+                print(blist[i], end = ' ')
+            print("\nScore: " + str(scoreboard))
+            board = str(input("\nIncorrect!\nYou have " + str(guesses) + " incorrect guesses left\n"))
+    if guesses == 0:
+        print("\nO->-< \nFAILURE, the word was " + solution)
+        print("\nScore: " + str(scoreboard))
+
 wordamount = -1
-scoreboard = 0
 while wordamount != 0:
     wordamount = random.randint(2,8)
     if wordamount == 0:
@@ -93,41 +132,5 @@ while wordamount != 0:
             print("_ ", end = '')
     blist = list(bord)
     board = str(input("\nGuess a letter:\nYou have " + str(guesses) + " incorrect guesses left\n"))
-    while guesses > 0:
-        if len(board) == 1:
-            if board in solution:
-                scoreboard = scores(board, scoreboard)
-                print("\nScore: " + str(scoreboard))
-                solspt = list(solution)
-                for va in range(0, len(solspt)):
-                    if board == solspt[va]:
-                        blist[va] = board
-                guess(guesses)
-                for i in range(0, len(blist)):
-                    print(blist[i], end = ' ')
-                if "_" not in blist:
-                    print("success")
-                    break
-                print("\nScore: " + str(scoreboard))
-                board = str(input("\nGuess a letter:\nYou have " + str(guesses) + " incorrect guesses left\n"))
-            else:
-                guesses -= 1
-                guess(guesses)
-                for i in range(0, len(blist)):
-                    print(blist[i], end = ' ')
-                if guesses == 0:
-                    break
-                else:
-                    print("\nScore: " + str(scoreboard))
-                    board = str(input("\nIncorrect!\nYou have " + str(guesses) + " incorrect guesses left\n"))
-        else:
-            guesses -= 1
-            for i in range(0, len(blist)):
-                print(blist[i], end = ' ')
-            print("\nScore: " + str(scoreboard))
-            board = str(input("\nIncorrect!\nYou have " + str(guesses) + " incorrect guesses left\n"))
-
-    if guesses == 0:
-        print("\nO->-< \nFAILURE, the word was " + solution)
-        print("\nScore: " + str(scoreboard))
+    scoretable(board, solution, blist, guesses)
 
